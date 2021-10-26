718db6c5897c6d6df943ed9184977d4c5eba1b07
Dario999 <darioivanovski33@gmail.com>
2021-10-26 19:33:52 +0200

    data binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,4 +6,14 @@ import { Component } from "@angular/core";
 })
 export class ServerComponent {
 
+    serverId: number = 10;
+    serverStatus: string = 'offline';
+
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
+
+    getServerStatus(): string {
+        return this.serverStatus;
+    }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  //selector: '[app-servers]',
+  //selector: '.app-servers',
   selector: 'app-servers',
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>
+  // `,
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  //styleUrls: ['./servers.component.css']
+  styles: [`
+    h3 {
+      color: dodgerblue;
+    }
+  `]
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'Testserver';
+  serverCreated = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },2000)
+   }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer(){
+    this.serverCreated = true;
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event){
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
